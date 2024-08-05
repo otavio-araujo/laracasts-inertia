5a3d6c61de586cb35cdc8da60ef78ae2294759da
otavio-araujo <otavio_araujo@hotmail.com>
2024-08-04 22:07:11 -0300

    Perform Non-GET Requests

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -3,16 +3,19 @@ import {Link} from "@inertiajs/vue3";
 </script>
 
 <template>
-    <nav>
-        <ul>
+    <nav class="mt-6">
+        <ul class="list-disc">
             <li>
-                <Link href="/">Home</Link>
+                <Link class="text-blue-500 hover:underline" href="/">Home</Link>
             </li>
             <li>
-                <Link href="/users">Users</Link>
+                <Link class="text-blue-500 hover:underline" href="/users">Users</Link>
             </li>
             <li>
-                <Link href="/settings">Settings</Link>
+                <Link class="text-blue-500 hover:underline" href="/settings">Settings</Link>
+            </li>
+            <li>
+                <Link as="button" class="text-blue-500 hover:underline" href="/logout" method="post">Log Out</Link>
             </li>
         </ul>
     </nav>
